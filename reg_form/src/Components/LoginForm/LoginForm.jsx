5a025934143e6400b797b8ec650d1b2adf0ea927
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-05-09 20:39:06 +0400

    Update src

--- reg_form/src/Components/LoginForm/LoginForm.jsx
+++ reg_form/src/Components/LoginForm/LoginForm.jsx
@@ -9,14 +9,17 @@ const LoginForm = () => {
         <div className="wrapper">
             <form action="">
                 <h1>Admissions Form</h1>
+                <div className="topic">Student Name</div>
                 <div className="input-box">
                     <input type="text" placeholder="Name" required />
                     <FaRegUser className="icon" />
                 </div>
+                <div className="topic">Email Address</div>
                 <div className="input-box">
                     <input type="email" placeholder="Email" required />
                     <MdOutlineMailLock className="icon"/>
                 </div>
+                <div className="topic">Phone Number</div>
                 <div className="input-box">
                     <input type="text" placeholder="Phone" required />
                     <MdOutlineLocalPhone className="icon"/>
