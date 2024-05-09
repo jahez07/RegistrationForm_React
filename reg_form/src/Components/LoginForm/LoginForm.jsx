d772b9ddb89acc8e3d338fc37b6b18d6c1df52f3
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-05-09 19:37:05 +0400

    Update node_modules

--- reg_form/src/Components/LoginForm/LoginForm.jsx
+++ reg_form/src/Components/LoginForm/LoginForm.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import './LoginForm.css';
+import { MdOutlineMailLock } from "react-icons/md";
 
 const LoginForm = () => {
     return(
-        <div>LoginForm</div>
+        <div className="wrapper">
+            <form action="">
+                <h1>Admissions Form</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Name" required />
+                </div>
+                <div className="input-box">
+                    <input type="email" placeholder="Email" required />
+                </div>
+                <div className="input-box">
+                    <input type="text" placeholder="Phone" required />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
     );
 };
 
